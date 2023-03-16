8f8766689cfbfc792a4e81d857b7a97a937b350f
Julian <juliansoyhenry@outlook.com>
2023-03-16 14:55:07 -0600

    Homework done introToCs

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,37 +1,63 @@
 "use strict";
 
 function BinarioADecimal(num) {
-  // tu codigo aca
-  let res = 0
-  let array = []
+  var content = num
 
-  for (let i = 0; i < num.length; i++){
-    array.unshift(num[i])
+  var createArray = content.split('')
+  var count = 0
+  var sum = 0
+
+  for(var i = createArray.length-1; i >= 0; i--) {
+      var operation = Math.pow(2, count)*createArray[i]
+      sum += operation
+
+      count = count + 1
   }
-  for (let i = 0; i < array.length; i++){
-    res = res + Math.pow(2,i) * array[i]
-  }
-  return res
+
+  return sum
+  // tu codigo aca
+  // let res = 0
+  // let array = []
+
+  // for (let i = 0; i < num.length; i++){
+  //   array.unshift(num[i])
+  // }
+  // for (let i = 0; i < array.length; i++){
+  //   res = res + Math.pow(2,i) * array[i]
+  // }
+  // return res
 }
 
 
 function DecimalABinario(num) {
+  var content2 = num
+  var createArray2 = []
+
+  for(var i = content2; i >= 1; i = i / 2) {
+
+      //operacion = (i / 2) 
+      var roundedFloor = Math.floor(i)
+      var moduleOperation = roundedFloor % 2
+      createArray2.unshift(moduleOperation)
+  }
+  var conCat = createArray2.toString().replaceAll(',','')
+  return conCat
   // tu codigo aca
-  let array = []
-  let str = ""
-  for (let i = 0; i < 9999; i++){
-    if (num < 1){
-      break;
-    }
-    let a = num % 2;
-    array.unshift(a);
-    num = Math.floor(num / 2)
- 
-}
-  for (let i = 0; i < array.length; i++){
-    str = str + array[i]
-}
-return str
+  //   let array = []
+  //   let str = ""
+  //   for (let i = 0; i < 9999; i++){
+  //     if (num < 1){
+  //       break;
+  //     }
+  //     let a = num % 2;
+  //     array.unshift(a);
+  //     num = Math.floor(num / 2)
+  
+  // }
+  //   for (let i = 0; i < array.length; i++){
+  //     str = str + array[i]
+  // }
+  // return str
 }
 
 module.exports = {
